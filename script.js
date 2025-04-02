20dce8def30c86a547c97f7f5b7828bcc808e6fd
asaboi2 <129332648+asaboi2@users.noreply.github.com>
2025-04-01 23:13:51 -0600

    Update script.js

--- script.js
+++ script.js
@@ -84,7 +84,7 @@ document.addEventListener('DOMContentLoaded', () => {
         'Ahi Crudo': { image: 'assets/ahi crudo.png', price: 20, category: 'Appetizers', prepTime: 3 },
         'Whipped Ricotta': { image: 'assets/ricotta.png', price: 14, category: 'Appetizers', prepTime: 2 },
         'Raviolo al Uovo': { image: 'assets/raviolo.png', price: 8, category: 'Appetizers', prepTime: 2.5 },
-        'Prosciutto e Melone': { image: 'assets/prosciutto-e-melone.png', price: 10, category: 'Appetizers', prepTime: 1.5 },
+        'Prosciutto e Melone': { image: 'assets/prosciutto e melone.png', price: 10, category: 'Appetizers', prepTime: 1.5 },
         'Crispy Gnudi': { image: 'assets/crispy gnudi.png', price: 12, category: 'Appetizers', prepTime: 3.5 },
         'Marinated Olives': { image: 'assets/olives.png', price: 6, category: 'Appetizers', prepTime: 1 },
         'House Salad': { image: 'assets/house salad.png', price: 12, category: 'Salads', prepTime: 2.5 },
